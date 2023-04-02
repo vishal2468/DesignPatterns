899fcef8908394371e4002db069ccaeeccd05e3b
vishal2468 <vishalpoddar2468@gmail.com>
2023-04-02 15:32:12 +0530

    added singleton and immutable

---
+++ oops-singleton/Singleton.java
@@ -0,0 +1,12 @@
+class Singleton {
+
+    private static final Singleton instance = new Singleton();
+
+    private Singleton() {
+    }
+
+    public static Singleton getSingleton() {
+        return instance;
+    }
+    
+}
